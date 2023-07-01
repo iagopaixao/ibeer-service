90858c3ceb351c8b8ac6c954f12c34e328cc7f74
Iago Paixão <euiagopaixao@gmail.com>
2023-07-01 20:57:34 -0300

    fix: adds app context load assertion in IBeerApplicationTests

--- src/test/java/com/ipaixao/ibeer/IBeerApplicationTests.java
+++ src/test/java/com/ipaixao/ibeer/IBeerApplicationTests.java
@@ -1,11 +1,23 @@
 package com.ipaixao.ibeer;
 
+import jakarta.servlet.ServletContext;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class IBeerApplicationTests {
 
   @Test
-  void contextLoads() {}
+  @DisplayName("should check if application context loaded successfully")
+  void shouldVerifycontextLoads(ApplicationContext appContext) {
+
+    assertThat(appContext).isNotNull();
+  }
 }
